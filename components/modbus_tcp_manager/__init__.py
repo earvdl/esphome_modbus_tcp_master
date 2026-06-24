f9396ce23f768f3ebad56273746208cb49ee4222
earvdl <earvdl@pm.me>
2026-06-24 13:14:17 +0200

    Added missing constants

--- components/modbus_tcp_manager/__init__.py
+++ components/modbus_tcp_manager/__init__.py
@@ -10,28 +10,30 @@ CONF_WATCHDOG_REGISTER = "watchdog_register"
 CONF_WATCHDOG_INTERVAL = "watchdog_interval"
 CONF_SAFE_MODE_REGISTERS = "safe_mode_registers"
 
+# Shared constants used by sensor.py
+CONF_MODBUS_TCP_ID = "modbus_tcp_id"
+CONF_REGISTER_ADDRESS = "register_address"
+CONF_FUNCTION_CODE = "function_code"
+
 # Namespace
 modbus_tcp_ns = cg.esphome_ns.namespace("modbus_tcp")
 ModbusTCPManager = modbus_tcp_ns.class_("ModbusTCPManager", cg.Component)
 
-# Dependencies
 DEPENDENCIES = ["network"]
 CODEOWNERS = ["@Gucioo"]
 
-# Safe mode register schema
 SAFE_MODE_REGISTER_SCHEMA = cv.Schema({
     cv.Required("register"): cv.positive_int,
     cv.Required("value"): cv.int_range(min=-32768, max=32767),
 })
 
-# Configuration schema
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ModbusTCPManager),
     cv.Required(CONF_HOST): cv.string,
     cv.Optional(CONF_PORT, default=502): cv.port,
     cv.Optional(CONF_UNIT_ID, default=1): cv.int_range(min=1, max=255),
     cv.Optional(CONF_WATCHDOG_REGISTER): cv.positive_int,
-    cv.Optional(CONF_WATCHDOG_INTERVAL, default="10s"): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_WATCHDOG_INTERVAL): cv.positive_time_period_milliseconds,
     cv.Optional(CONF_SAFE_MODE_REGISTERS, default=[]): cv.All(cv.ensure_list(SAFE_MODE_REGISTER_SCHEMA)),
 }).extend(cv.COMPONENT_SCHEMA)
 
@@ -42,20 +44,13 @@ async def to_code(config):
         config[CONF_PORT],
         config[CONF_UNIT_ID]
     )
-    
-    # Add watchdog configuration if specified
-    # if CONF_WATCHDOG_REGISTER in config:
-    #     cg.add(var.set_watchdog_register(config[CONF_WATCHDOG_REGISTER]))
-    #     cg.add(var.set_watchdog_interval(config[CONF_WATCHDOG_INTERVAL]))
-    # inside to_code(config): suggested by CoPilot
+
     if CONF_WATCHDOG_REGISTER in config:
         cg.add(var.set_watchdog_register(config[CONF_WATCHDOG_REGISTER]))
-    
-    if CONF_WATCHDOG_INTERVAL in config:
-        cg.add(var.set_watchdog_interval(config[CONF_WATCHDOG_INTERVAL]))
-    
-    # Add safe mode registers if specified
+        if CONF_WATCHDOG_INTERVAL in config:
+            cg.add(var.set_watchdog_interval(config[CONF_WATCHDOG_INTERVAL]))
+
     for safe_reg in config[CONF_SAFE_MODE_REGISTERS]:
         cg.add(var.add_safe_mode_register(safe_reg["register"], safe_reg["value"]))
-    
+
     await cg.register_component(var, config)
